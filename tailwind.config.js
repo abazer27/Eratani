375538821dc7462e031b5de6cfa8a058a9cb5246
Akbar Hazura <akbarhazura@gmail.com>
2022-03-22 23:29:29 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        "poppins-bold": ["Poppins-Bold", "sans-serif"],
+      },
+    },
+    letterSpacing:{
+      wide:'3px'
+    }
+  },
+  plugins: [],
+}
